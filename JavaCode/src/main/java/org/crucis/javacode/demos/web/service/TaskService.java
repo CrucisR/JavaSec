9547f4716e5abbd2dc6cc9b117f124f2707cdadd
CrucisR <84627094+CrucisR@users.noreply.github.com>
2024-05-25 03:15:27 +0800

    Add files via upload

---
+++ JavaCode/src/main/java/org/crucis/javacode/demos/web/service/TaskService.java
@@ -0,0 +1,9 @@
+package org.crucis.javacode.demos.web.service;
+
+import org.crucis.javacode.demos.web.vo.TaskVO;
+
+public class TaskService {
+    public CommonResponse<String> createTask(TaskVO tVo) {
+        return null;
+    }
+}
---
+++ JavaCode/src/main/java/org/crucis/javacode/demos/web/vo/TaskVO.java
@@ -0,0 +1,13 @@
+package org.crucis.javacode.demos.web.vo;
+
+import java.util.Date;
+
+public class TaskVO {
+    // Date类型
+    private Date createTime;
+    // 自定义数据类型
+    private String taskName;
+    // 整数
+    private Integer taskId;
+
+}
